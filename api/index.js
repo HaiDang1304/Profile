3a05dee9b96ac63f644cc80750e8450b9b1d387a
HaiDang1304 <haidanglu2004@gmail.com>
2026-09-02 17:24:23 +0700

    debug: wrap api require in try catch

--- api/index.js
+++ api/index.js
@@ -1,2 +1,13 @@
-const app = require('../server/index.js');
-module.exports = app;
+try {
+  const app = require('../server/index.js');
+  module.exports = app;
+} catch (error) {
+  module.exports = (req, res) => {
+    res.status(500).json({
+      message: 'Failed to load server/index.js',
+      error: error.message,
+      stack: error.stack,
+      name: error.name
+    });
+  };
+}
