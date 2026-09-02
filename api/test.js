1064605cb6069fc45b9ecabcd823bb4afc3b8596
HaiDang1304 <haidanglu2004@gmail.com>
2026-09-02 17:28:36 +0700

    debug: add db_name to test

--- api/test.js
+++ api/test.js
@@ -1,6 +1,7 @@
 module.exports = (req, res) => {
   res.json({
     env_db_host: process.env.DB_HOST || 'MISSING',
+    env_db_name: process.env.DB_NAME || 'MISSING',
     node_version: process.version,
     message: 'Hello from test API!'
   });
